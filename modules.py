97ac520cb56be7062abf237a78e207c964042e11
Talha ahmad awan <zablerkhanahmad@gmail.com>
2026-02-10 16:37:36 +0500

    first code of python and modules install using pips

---
+++ modules.py
@@ -0,0 +1,16 @@
+import pyjokes
+# print("Here is a joke for you:")
+joke = pyjokes.get_joke()
+print(joke)                      
+
+
+
+# two types of comment
+
+
+# single line comment
+
+"""This is
+ a multiline 
+ comment."""
+""" line comment """
